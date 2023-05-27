4805d7ce2ab9d14b1cc19e9cf6d047eb5285bf7d
musamarabbani <usama.rabbani@invozone.com>
2023-05-27 16:03:42 +0500

    use reducer hook implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const initialState = { count: 0 };
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'Increment':
+			return { ...state, count: state.count + 1 };
+		case 'Decrement':
+			return { ...state, count: state.count - 1 };
+		default:
+			return state;
+	}
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [updatedCount, dispatch] = useReducer(reducer, initialState);
+	return (
+		<div className='App'>
+			<h4>Current Count: {updatedCount.count}</h4>
+			<button onClick={() => dispatch({ type: 'Increment' })}>Increment</button>
+			<button onClick={() => dispatch({ type: 'Decrement' })}>Decrement</button>
+		</div>
+	);
 }
 
 export default App;
